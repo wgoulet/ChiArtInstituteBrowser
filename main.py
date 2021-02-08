3d5e89480d79b002fee27c901045da5829c17ee4
Walter Goulet <wgoulet@gmail.com>
2021-02-07 23:23:27 -0600

    Added support for creating video files with audio tour recordings

--- main.py
+++ main.py
@@ -5,13 +5,19 @@ import time
 import json
 import urllib.parse
 import csv
+from PIL import Image
+import re
+import math
+import moviepy.editor
+import moviepy.Clip
+import mutagen
 
 def main():
     print ("hello world")
     headers = {'user-agent': 'art-institute-browse/wgoulet@gmail.com'}
 
     # search only for individual artists
-    artist = "degas"
+    artist = "van gogh"
     searchqry = {
         "query": {
             "bool": {
@@ -59,7 +65,7 @@ def main():
     r.raise_for_status()
     art = r.json()
     resultsize = art['pagination']['total']
-    pages = int(resultsize / 10)
+    pages = int(math.ceil(resultsize / 10))
     
     for i in range(1,pages):
         url = "https://api.artic.edu/api/v1/artworks/search?&limit=10&page={0}".format(i)
@@ -69,6 +75,49 @@ def main():
         for item in art['data']:
             print(item['title']) 
             print(item['id'])
+            # Try and find an audio stop file for the artwork
+            searchqry = {
+                "query": {
+                    "bool": {
+                        "must": {
+                            "term": {
+                                "artwork_ids": item['id']
+                            }
+                        }
+                    }
+                }
+            }
+            time.sleep(1)
+            url = "https://api.artic.edu/api/v1/sounds/search?limit=10"
+            r = requests.post(url,json=searchqry,headers=headers)
+            r.raise_for_status()
+            sounds = r.json()
+            audlen = 0
+            audfname = None
+            audfclip = None
+            for sound in sounds['data']:
+                if re.match('audio stop.*',sound['title'],re.IGNORECASE):
+                    pprint.pprint(sound['title'])
+                    url = sound['api_link']
+                    time.sleep(1)
+                    r = requests.get(url,headers=headers)
+                    r.raise_for_status()
+                    soundinfo = r.json()
+                    url = soundinfo['data']['content']
+                    time.sleep(1)
+                    r = requests.get(url,headers=headers)
+                    r.raise_for_status()
+                    audio = r.content
+                    audfname = "{0}-{1}-audiostop.mp3".format(artist,item['title'])
+                    file = open(audfname,"wb")
+                    file.write(audio)
+                    file.close()
+                    audfclip = moviepy.editor.AudioFileClip(audfname,fps= 44100)
+                    audfclip.close()
+                    audinfo = mutagen.File(audfname)
+                    audlen = audinfo.info.length
+
+
             imagesearch = "https://api.artic.edu/api/v1/artworks/{0}?fields=id,title,image_id".format(item['id'])
             r = requests.get(imagesearch,headers=headers)
             r.raise_for_status()
@@ -83,8 +132,17 @@ def main():
                 img = r.content
                 file = open("{0}-{1}.jpg".format(artist,item['title']),"wb")
                 file.write(img)
+                file.close()
+                clip = moviepy.editor.ImageClip("{0}-{1}.jpg".format(artist,item['title']),"rb")
+                clip.duration = int(math.ceil(audlen))
+                vclip = clip.set_duration(int(math.ceil(audlen)))
+                audio = moviepy.editor.AudioFileClip(audfname)
+                final = vclip.set_audio(audio)
+                final.write_videofile("{0}-{1}.mp4".format(artist,item['title']),
+                    codec= 'mpeg4',fps=30,audio=True)
             except:
                 print("Unable to get image for {0}".format(item['title']))
+                raise
 
             time.sleep(1)
 
