a7ffd397a65d4357aac1beaa9ee744ecc771e207
Walter Goulet <wgoulet@gmail.com>
2021-02-24 21:52:34 -0600

    Got basic streaming working with flask

--- videodb.py
+++ videodb.py
@@ -15,126 +15,132 @@ from io import FileIO
 from pathlib import Path
 from typing import Optional
 import ffmpeg
+import flask
+from flask.helpers import make_response
 import mutagen
-import requests
-from fastapi import Depends, FastAPI, HTTPException, status
-from fastapi.middleware.cors import CORSMiddleware
+#from fastapi import Depends, FastAPI, HTTPException, status
+#from fastapi.middleware.cors import CORSMiddleware
 from PIL import Image
 from pydantic import BaseModel
-from starlette.responses import HTMLResponse, RedirectResponse, JSONResponse, Response
+#from starlette.responses import HTMLResponse, RedirectResponse, JSONResponse, Response
 import uvicorn
-from fastapi.responses import StreamingResponse
+#from fastapi.responses import StreamingResponse
 import io
+from flask import Flask, request
+from flask import Response
+import flask.cli
 
 class Artwork():
-    title: str
-    arttype: str
-    artist: str
-    image: Optional[str] = None
-    video: Optional[bytes] = None
+	title: str
+	arttype: str
+	artist: str
+	image: Optional[str] = None
+	video: Optional[bytes] = None
 
 def main(argv):
-    print("hello world")
+	print("hello world")
  
-    connection = sqlite3.connect("artinstvid.db",detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
-    cursor = connection.cursor()
+	connection = sqlite3.connect("artinstvid.db",detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+	cursor = connection.cursor()
 
-    cursor.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='artwork'")
+	cursor.execute("SELECT count(name) FROM sqlite_master WHERE type='table' AND name='artwork'")
 
-    if cursor.fetchone()[0]!=1:
-        print("Creating table")
-        cursor.execute("CREATE TABLE artwork (title TEXT, arttype TEXT, \
-            artist TEXT, image BLOB, video BLOB)")
-        connection.commit()
-    item = Artwork()
-    with open("Edward Hopper-Nighthawks.jpg","rb") as imgfile:
-        item.image = imgfile.read()
-    with open("edward hopper-Nighthawks-all.mp4","rb") as vidfile:
-        item.video = vidfile.read()
-    cursor = connection.cursor()
-    item.artist = "Edward Hopper"
-    item.title = "Nighthawks"
-    item.arttype = "Painting"
-    record = (item.artist,item.title,item.arttype,item.image,item.video)
-    cursor.execute("INSERT INTO artwork (artist, title, arttype, image, video) VALUES (?,?,?,?,?)",record)
-    connection.commit()
+	if cursor.fetchone()[0]!=1:
+		print("Creating table")
+		cursor.execute("CREATE TABLE artwork (title TEXT, arttype TEXT, \
+			artist TEXT, image BLOB, video BLOB)")
+		connection.commit()
+	item = Artwork()
+	with open("Edward Hopper-Nighthawks.jpg","rb") as imgfile:
+		item.image = imgfile.read()
+	with open("edward hopper-Nighthawks-all.mp4","rb") as vidfile:
+		item.video = vidfile.read()
+	cursor = connection.cursor()
+	item.artist = "Edward Hopper"
+	item.title = "Nighthawks"
+	item.arttype = "Painting"
+	record = (item.artist,item.title,item.arttype,item.image,item.video)
+	cursor.execute("INSERT INTO artwork (artist, title, arttype, image, video) VALUES (?,?,?,?,?)",record)
+	connection.commit()
 
-    cursor.execute("SELECT image from artwork")
-    timage = cursor.fetchone()[0]
-    with open("test.jpg","wb") as testimg:
-        testimg.write(timage)
-    cursor.execute("SELECT video from artwork")
-    tvideo = cursor.fetchone()[0]
-    with open("test.mp4","wb") as testvid:
-        testvid.write(tvideo)
+	cursor.execute("SELECT image from artwork")
+	timage = cursor.fetchone()[0]
+	with open("test.jpg","wb") as testimg:
+		testimg.write(timage)
+	cursor.execute("SELECT video from artwork")
+	tvideo = cursor.fetchone()[0]
+	with open("test.mp4","wb") as testvid:
+		testvid.write(tvideo)
 
-    app = FastAPI()
-
-    origins = [
-    "http://localhost",
-    "http://localhost:3000",
-    ]
-    app.add_middleware(
-        CORSMiddleware,
-        allow_origins=origins,
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
-    )
-
-    @app.route("/list")
-    def read_root(request):
-        return JSONResponse({"you are": "authenticated"})
+	app = Flask(__name__)
 
 
-    @app.route("/vidtest")
-    def read_vid(request):
-        connection = sqlite3.connect("artinstvid.db",detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
-        cursor = connection.cursor()
-        cursor.execute("SELECT video from artwork")
-        tvideo = cursor.fetchone()[0]
-        connection.close()
-        #tvideo = open("test.mp4","rb")
-        size = len(tvideo)
-        # Let the client know we support content ranges
-        if 'Range' not in request.headers:
-            headers = {
-                "Accept-Ranges": "bytes"
-            }
-            return Response(content=None, media_type="video/mp4",status_code=206,headers=headers)
-        # From https://dev.to/singhpratyush/the-taste-of-media-streaming-with-flask-58a4
-        m = re.search('(\d+)-(\d*)', request.headers['range'])
-        g = m.groups()
-        byte1, byte2 = 0, None
-        if g[0]:
-            byte1 = int(g[0])
-        if g[1]:
-            byte2 = int(g[1])
-        length = size - byte1
-        if byte2:
-            length = byte2 + 1 - byte1
-        stream = io.BytesIO(tvideo[byte1: byte2])
-        headers = {
-            "Accept-Ranges": "bytes",
-            "Content-Length": F"{len(tvideo)}",
-            #"Content-Range": F"bytes {asked}-{sz-1}/{sz}",
-            "Content-Range" : F"bytes {byte1}-{byte1 + length - 1}/{size}"
-        }
-        #response =  StreamingResponse(streaming_file(file_path, CS, asked), headers=headers,
-        return StreamingResponse(stream, media_type="video/mp4",status_code=206,headers=headers)
+	@app.route("/list")
+	def read_root():
+		make_response()
+		response = make_response(({"you are": "authenticated"},200))
+		return response
+		return flask.Response(response,status=200)
+		#return JSONResponse({"you are": "authenticated"})
 
-    @app.route("/imgtest")
-    def read_img(request):
-        connection = sqlite3.connect("artinstvid.db",detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
-        cursor = connection.cursor()
-        cursor.execute("SELECT image from artwork")
-        tvideo = cursor.fetchone()[0]
-        connection.close()
-        stream = io.BytesIO(tvideo)
-        #tvideo = open("test.mp4","rb")
-        return StreamingResponse(stream, media_type="image/jpeg")
+	@app.after_request
+	def after_request(response):
+		response.headers.add('Accept-Ranges', 'bytes')
+		return response
 
-    uvicorn.run(app, host='127.0.0.1', port=8000)
+	# From https://stackoverflow.com/questions/57314357/streaming-video-files-using-flask
+	def get_chunk(byte1=None, byte2=None):
+		start = 0
+		length = 102400
+		connection = sqlite3.connect("artinstvid.db",detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+		cursor = connection.cursor()
+		cursor.execute("SELECT video from artwork")
+		tvideo = cursor.fetchone()[0]
+		connection.close()
+		file_size = len(tvideo)
+		if byte1 < file_size:
+			start = byte1
+		if byte2:
+			length = byte2 + 1 - byte1
+		else:
+			length = file_size - start
+		stream = io.BytesIO(tvideo)
+		stream.seek(start)
+		chunk = stream.read(length)
+		stream.close()
+		return chunk, start, length, file_size
+
+
+	@app.route("/vidtest")
+	def read_vid():
+		range_header = request.headers.get('Range', None)
+		byte1, byte2 = 0, None
+		if range_header:
+			match = re.search(r'(\d+)-(\d*)', range_header)
+			groups = match.groups()
+
+			if groups[0]:
+				byte1 = int(groups[0])
+			if groups[1]:
+				byte2 = int(groups[1])
+		chunk, start, length, file_size = get_chunk(byte1, byte2)
+		resp = Response(chunk,206,mimetype='video/mp4',
+			content_type='video/mp4',direct_passthrough=True)
+		resp.headers.add('Content-Range', 'bytes {0}-{1}/{2}'.format(start, start + length - 1, file_size))
+		return resp
+
+	@app.route("/imgtest")
+	def read_img(request):
+		connection = sqlite3.connect("artinstvid.db",detect_types=sqlite3.PARSE_DECLTYPES|sqlite3.PARSE_COLNAMES)
+		cursor = connection.cursor()
+		cursor.execute("SELECT image from artwork")
+		tvideo = cursor.fetchone()[0]
+		connection.close()
+		stream = io.BytesIO(tvideo)
+		#tvideo = open("test.mp4","rb")
+		return StreamingResponse(stream, media_type="image/jpeg")
+
+	app.run(host="127.0.0.1",port=8080)
 
 if __name__ == "__main__":
 	main(sys.argv)
